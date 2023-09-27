5375e03512309a861ac7b7c725ba77dfb42deb0d
anonymity678 <wenhan.cao@tum.de>
2023-09-27 21:06:13 +0200

    modified:   README.md 	new file:   SinSystem/Matern/plot_J.py 	new file:   SinSystem/Matern/plot_u.py 	new file:   SinSystem/Matern/sin_Matern_4_J.pdf 	new file:   SinSystem/Matern/sin_Matern_4_u.pdf 	new file:   SinSystem/Matern/sin_Matern_4_w.pdf 	new file:   SinSystem/trapz/plot_J.py 	new file:   SinSystem/trapz/plot_u.py 	new file:   SinSystem/trapz/sin_trapz_J.pdf 	new file:   SinSystem/trapz/sin_trapz_u.pdf 	new file:   SinSystem/trapz/sin_trapz_w.pdf 	new file:   ThirdOrderSystem/Matern/plot_J.py 	new file:   ThirdOrderSystem/Matern/plot_K.py 	new file:   ThirdOrderSystem/Matern/tos_Matern_4_J.pdf 	new file:   ThirdOrderSystem/Matern/tos_Matern_4_K.pdf 	new file:   ThirdOrderSystem/Matern/tos_Matern_4_w.pdf 	new file:   ThirdOrderSystem/trapz/plot_J.py 	new file:   ThirdOrderSystem/trapz/plot_K.py 	new file:   ThirdOrderSystem/trapz/tos_trapz_J.pdf 	new file:   ThirdOrderSystem/trapz/tos_trapz_K.pdf 	new file:   ThirdOrderSystem/trapz/tos_trapz_w.pdf

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # Impact of Computation in Integral Reinforcement Learning for Continuous-Time Control
-This is the implementation of the paper "Impact of Computation in Integral Reinforcement Learning for Continuous Time Control" submitted to ICLR 2024.
-## 
+
+This repository explores the impact of computation in integral reinforcement learning for continuous-time control systems. It contains various Python scripts that demonstrate different methods and approaches.
+
+## Environment Requirements
+
+- **Python**
+- **Libraries**:
+  - `numpy`
+  - `matplotlib`
+  - `scipy`
+  - `probnum`
+  - `sklearn`
+  - `argparse`
+  - `os`
+
+## Examples
+
+1. **Sin System with Matern Kernel**
+   - **File**: `example_sin_Matern.py`
+   - **Description**: Demonstrates the use of the Matern kernel with the Sin System.
+
+2. **Sin System Motivation**
+   - **File**: `example_sin_motivation.py`
+   - **Description**: Provides motivation for using the Sin System in reinforcement learning.
+
+3. **Sin System with Trapezoidal Rule**
+   - **File**: `example_sin_trapz.py`
+   - **Description**: Uses the trapezoidal rule for integration with the Sin System.
+
+4. **Third Order System with Matern Kernel**
+   - **File**: `example_tos_Matern.py`
+   - **Description**: Demonstrates the use of the Matern kernel with a third-order system.
+
+5. **Third Order System with Trapezoidal Rule**
+   - **File**: `example_tos_trapz.py`
+   - **Description**: Uses the trapezoidal rule for integration with a third-order system.
+
+6. **Main Example**
+   - **File**: `main.py`
+   - **Description**: A primary example that showcases the application of the methods.
+
+## Subdirectories
+
+- **Illustration_of_BQ**: Contains scripts like `BQ_matern.py` and `BQ_wiener.py` that illustrate Bayesian quadrature methods.
+- **Motivation_Cartpole**: (Further exploration needed for a detailed description)
+- **SinSystem**: Contains resources related to the Sin System.
+- **ThirdOrderSystem**: Contains resources related to the Third Order System.
+- **Motivation_Sinsystem**: (Further exploration needed for a detailed description)
---
+++ SinSystem/Matern/plot_u.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import matplotlib.cm as cm
+from matplotlib.ticker import ScalarFormatter, FuncFormatter
+
+font_size = 30  # Define a common font size variable
+
+# Set the font to Arial
+plt.rcParams['font.sans-serif'] = "Arial"
+plt.rcParams['font.family'] = "sans-serif"
+
+# Custom formatter function
+def my_formatter(x, pos):
+    return "{:.0f}".format(x * 1e4)
+
+folder_name = "./"
+
+# 获取所有的npy文件
+file_list = os.listdir(folder_name)
+file_list = [f for f in file_list if f.endswith('.npy')]
+file_list.sort()  # 排序以确保文件以正确的顺序处理
+
+# 定义颜色列表
+colormap = cm.get_cmap("tab10", len(file_list))
+
+# 使用映射为每个文件生成颜色
+colors = [colormap(i) for i in range(len(file_list))]
+
+fig, ax = plt.subplots(figsize=(9, 6))
+
+u_seq = []
+sample_points_squared = []
+point_seq = []
+
+for file, color in zip(file_list, colors):
+    _, u, _ = np.load(os.path.join(folder_name, file), allow_pickle=True)
+    sample_point = int(file.split("_")[1].split(".")[0])
+
+    u_seq.append(u)
+    sample_points_squared.append(1.0 / (sample_point ** 4))  # Storing the square of sample_point
+    point_seq.append(sample_point)
+
+u_seq = np.array(u_seq)
+print(u_seq)
+sample_points_squared = np.array(sample_points_squared)
+point_seq = np.array(point_seq)
+
+# 设置线条宽度和标记大小
+ax.plot(sample_points_squared, u_seq, color='ForestGreen', linestyle='-', linewidth=2, alpha=0.8)
+ax.scatter(sample_points_squared, u_seq, color='ForestGreen', marker='P', s=100)
+# ax.plot(point_seq, J_seq, color='ForestGreen', linestyle='-', linewidth=2, alpha=0.8)
+# ax.scatter(point_seq, J_seq, color='ForestGreen', marker='P', s=100)
+
+ax.set_xlabel(r'${1}/{N^4}$', fontsize=font_size)
+ax.set_ylabel(r'$\mathbb{E}\left\{|\hat{u}^{(\infty)}-u^*|\right\}$', fontsize=font_size)
+ax.tick_params(axis='x', labelsize=font_size)
+ax.tick_params(axis='y', labelsize=font_size)
+
+yticks_original = np.array([1, 2, 3, 4]) * 1e-4
+ax.set_yticks(yticks_original)
+
+# xticks_original = np.linspace(4, 16, 4)
+# ax.set_xticks(xticks_original)
+
+# Using FuncFormatter to format y-axis ticks
+ax.yaxis.set_major_formatter(FuncFormatter(my_formatter))
+ax.annotate(r'$\times 10^{-4}$',
+            xy=(0.1, 1),
+            xycoords='axes fraction',
+            xytext=(0, 10),
+            textcoords='offset points',
+            fontsize=font_size,
+            ha='right',
+            va='bottom')
+
+plt.tight_layout()
+plt.savefig(f'./sin_Matern_4_u.pdf')
